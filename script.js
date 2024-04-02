35b27f28fb919a50379161a1d9a020a016b8e4a9
MikeKomari <michaelk.projects@gmail.com>
2024-04-02 23:25:52 +0700

    checkpoint

--- script.js
+++ script.js
@@ -3,9 +3,17 @@
 //CART POP UP LOGIC
 const cartButton = document.querySelector(".cart");
 const popUp = document.querySelector(".pop-up-wrapper");
+const container = document.querySelector(".container");
+const body = document.querySelector("body");
 
 cartButton.addEventListener("click", function (e) {
   popUp.classList.toggle("hidden");
+});
+
+container.addEventListener("click", function (e) {
+  if (e.target === container) {
+    popUp.classList.add("hidden");
+  }
 });
 
 //SLIDER LOGIC
@@ -33,6 +41,12 @@ imageFlag.addEventListener("click", function (e) {
 //pencet selain slider jadi tutup
 sliderWrapper.addEventListener("click", function (e) {
   if (e.target === sliderWrapper) {
+    closeModal();
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !sliderWrapper.classList.contains("hidden")) {
     closeModal();
   }
 });
@@ -70,4 +84,37 @@ counterMinus.addEventListener("click", function (e) {
     counterIndex -= 1;
     counterValue.textContent = counterIndex;
   }
+});
+
+//slider tapi mencet logic
+const smallThumbnail = document.querySelectorAll(".small_images");
+const bigThumbnail = document.querySelector(".big_image_tn");
+
+const bigImage1 = "images/image-product-1.jpg";
+const bigImage2 = "images/image-product-2.jpg";
+
+const mainFunctionChangingThumbnail = (e) => {
+  if (e.target.alt === "img1") {
+    bigThumbnail.innerHTML = `<img src="images/image-product-1.jpg" alt="imgBig1" />`;
+  } else if (e.target.alt === "img2") {
+    bigThumbnail.innerHTML = `<img src="images/image-product-2.jpg" alt="imgBig2" />`;
+  } else if (e.target.alt === "img3") {
+    bigThumbnail.innerHTML = `<img src="images/image-product-3.jpg" alt="imgBig3" />`;
+  } else if (e.target.alt === "img4") {
+    bigThumbnail.innerHTML = `<img src="images/image-product-4.jpg" alt="imgBig4" />`;
+  }
+};
+
+smallThumbnail.forEach((img) => {
+  img.addEventListener("mouseover", function (e) {
+    // console.log(e.target.alt);
+    mainFunctionChangingThumbnail(e);
+  });
+});
+
+smallThumbnail.forEach((img) => {
+  img.addEventListener("click", function (e) {
+    // console.log(e.target.alt);
+    mainFunctionChangingThumbnail(e);
+  });
 });
